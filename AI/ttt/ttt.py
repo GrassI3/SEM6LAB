7c0761e366e96decca03777f9b8a075bf191db9c
Jai Dessai <jaistudymail13@gmail.com>
2026-07-22 19:24:13 +0530

    simplified ttt

--- AI/ttt/ttt.py
+++ AI/ttt/ttt.py
@@ -1,85 +1,55 @@
-WIN = [ [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6] ]
+WIN = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
-def print_board(board):
-    for i in range(0, 9, 3):
-        print(" | ".join(board[i:i+3]))
-    print()
-
-def winner(board):
-    for a, b, c in WIN:
-        if board[a] == board[b] == board[c] != " ":
-            return board[a]
-    if " " not in board:
-        return "Draw"
+def winner(b):
+    for x,y,z in WIN:
+        if b[x] == b[y] == b[z] != " ":
+            return b[x]
+    if " " not in b:
+        return "D"
     return None
 
-def minimax(board, is_max):
-    result = winner(board)
+def minimax(b, turn):
+    w = winner(b)
+    if w == "X": return 1
+    if w == "O": return -1
+    if w == "D": return 0
 
-    if result == "X":
-        return 1
-    if result == "O":
-        return -1
-    if result == "Draw":
-        return 0
-
-    if is_max:
-        best = -999
-        for i in range(9):
-            if board[i] == " ":
-                board[i] = "X"
-                best = max(best, minimax(board, False))
-                board[i] = " "
-        return best
-    else:
-        best = 999
-        for i in range(9):
-            if board[i] == " ":
-                board[i] = "O"
-                best = min(best, minimax(board, True))
-                board[i] = " "
-        return best
-
-def ai_move(board):
-    best = 999
-    move = -1
-
+    best = -999 if turn == "X" else 999
     for i in range(9):
-        if board[i] == " ":
-            board[i] = "O"
-            score = minimax(board, True)
-            board[i] = " "
+        if b[i] == " ":
+            b[i] = turn
+            if turn == "X": best = max(best, minimax(b, "O"))
+            else: best = min(best, minimax(b, "X"))
+            b[i] = " "
+    return best
 
+def ai_move(b):
+    best = 999
+    move = 0
+    for i in range(9):
+        if b[i] == " ":
+            b[i] = "O"
+            score = minimax(b, "X")
+            b[i] = " "
             if score < best:
                 best = score
                 move = i
-
     return move
 
 board = [" "] * 9
 
 while winner(board) is None:
-    print_board(board)
+    for i in range(0,9,3):
+        print(" | ".join(board[i:i+3]))
 
-    pos = int(input("Enter position (1-9): ")) - 1
-    if board[pos] != " ":
-        print("Invalid move")
-        continue
-
-    board[pos] = "X"
+    board[int(input("Move (1-9): ")) - 1] = "X"
 
     if winner(board):
         break
 
     board[ai_move(board)] = "O"
 
-print_board(board)
+for i in range(0,9,3):
+    print(" | ".join(board[i:i+3]))
 
-result = winner(board)
-
-if result == "X":
-    print("You Win")
-elif result == "O":
-    print("AI Wins")
-else:
-    print("Draw")
+print("Winner:", winner(board))
